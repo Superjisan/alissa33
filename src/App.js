7a93542493b65719717783eec28af512f08fb12e
Jisan Zaman <jisan.zaman@dynamo.works>
2020-08-11 12:57:43 -0400

    fix ol error

--- src/App.js
+++ src/App.js
@@ -22,16 +22,18 @@ function App() {
           </p>
           <p>
             Some restrictions apply:
-            <ol>
-              <li>You are only allowed to use 1 coupon per day. Jisan can determine if you want to use more than one per day.</li>
-              <li>Coupons cannot be combined with other coupons, unless explicitly approved by Jisan.</li>
-              <li>You have to use all by 12AM August 13, 2021. No rollovers.</li>
-              <li>If accidentally pressed Redeem, let Jisan know and he can correct the error.</li>
-            </ol>
-
+          </p>
+          <ol>
+            <li>You are only allowed to use 1 coupon per day. Jisan can determine if you want to use more than one per day.</li>
+            <li>Coupons cannot be combined with other coupons, unless explicitly approved by Jisan.</li>
+            <li>You have to use all by 12AM August 13, 2021. No rollovers.</li>
+            <li>If accidentally pressed Redeem, let Jisan know and he can correct the error.</li>
+          </ol>
+          <p>
             Love you lots,
             Jisan
         </p>
+
         </Col>
       </Row>
       <CouponContainer />
